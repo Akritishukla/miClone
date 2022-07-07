d3171d228dbd99c4565dce06942d64d89d6f16c6
akriti23 <akriti14shukla@gmail.com>
2022-07-08 05:09:27 +0530

    first commit

---
+++ src/components/Videos.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import VideoCard from './VideoCard.js'
+import "../styles/Videos.css"
+
+const Videos = ({videos}) => {
+  return (
+    <div className='Videos'>
+    {videos.map((item,index)=>(
+        <VideoCard key={item.image} name={item.name} image={item.image} index={index}/> 
+
+    ))}
+    
+    </div>
+  )
+}
+
+export default Videos
